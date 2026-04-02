2c45214ecbcddd4ef2925e58295e2238f8d39333
Vítězslav Žáček <62467545+Vitek8@users.noreply.github.com>
2026-04-02 15:46:52 +0200

    odstranění všech vlastních řádků z main.m -> nutné manuální mazání souboru algorithms/report/data/index.mat pro správný běh

--- main.m
+++ main.m
@@ -81,7 +81,7 @@ clear ax ay cx cy i xs ys pgon start_position;
 
 w = waitforbuttonpress;
 % public_vars.h2 = figure;
-delete algorithms\report\assignment_2\data\index.mat
+% delete algorithms\report\assignment_2\data\index.mat
 
 while true
     % 1. Check if goal has been reached
