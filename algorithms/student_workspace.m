30c3c0afa5061c1cb0cd307811857d836c310d34
Vítězslav Žáček <62467545+Vitek8@users.noreply.github.com>
2026-04-04 14:59:38 +0200

    přidán výpočet na polohu cíle z read_only_vars.discrete_map.goal

--- algorithms/student_workspace.m
+++ algorithms/student_workspace.m
@@ -4,27 +4,23 @@ function [public_vars] = student_workspace(read_only_vars,public_vars)
 
 run('setup.m')
 
-
-start_pos_x = start_position(1);
-start_pos_y = start_position(2);
-goal_pos_x = 25;
-goal_pos_y = 10;
+goal_position = 0.2 * (read_only_vars.discrete_map.goal - 1);
 
 n = 100;
-x = linspace(start_pos_x, goal_pos_x, n);
-y = linspace(start_pos_y, goal_pos_y, n);
+x = linspace(start_position(1), goal_position(1), n);
+y = linspace(start_position(2), goal_position(2), n);
 
 freq = 0.05;
 sine_amplitude = 5;
 sine_count = 1;
 
-t = start_pos_x + linspace(0, sine_count / freq, 100000)';
-sine_y = start_pos_y + sine_amplitude * sin(2 * pi * freq * (t - start_pos_x));
+t = start_position(1) + linspace(0, sine_count / freq, 100000)';
+sine_y = start_position(2) + sine_amplitude * sin(2 * pi * freq * (t - start_position(1)));
 
 circular_arc_angles = linspace(pi, 0, 1000);
-radius = pi * freq * sine_count;
-circular_arc_x = start_pos_x + radius + radius * cos(circular_arc_angles)';
-circular_arc_y = start_pos_y + radius * sin(circular_arc_angles)';
+radius = 1/(2*freq);
+circular_arc_x = start_position(1) + radius + radius * cos(circular_arc_angles)';
+circular_arc_y = start_position(2) + radius * sin(circular_arc_angles)';
 
 straight_line = [x' y'];
 circular_arc = [circular_arc_x circular_arc_y];
@@ -32,8 +28,8 @@ sine_wave = [t sine_y];
 
 public_vars.path = sine_wave;
 
-calculated_start_x = goal_pos_x - 1/freq * sine_count;
-calculated_start_y = goal_pos_y;
+calculated_start_x = goal_position(1) - 1/freq * sine_count;
+calculated_start_y = goal_position(2);
 fprintf("Nastav počátek na: [%f %f]\n", calculated_start_x, calculated_start_y);
 
 % 8. Perform initialization procedure
