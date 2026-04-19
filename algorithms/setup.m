1c349a59dbd7b91a2e6efc015b80509a6c13cb8c
Vítězslav Žáček <62467545+Vitek8@users.noreply.github.com>
2026-04-19 13:51:25 +0200

    přidána implementace pro volbu trajektrorie pomocí public_vars.path_type

--- algorithms/setup.m
+++ algorithms/setup.m
@@ -1,3 +1,6 @@
-start_position = [22, 10, 0]; % (x, y, theta)
+start_position = [1, 6, 0]; % (x, y, theta)
 
-map_name = 'maps/custom_1.txt';
+map_name = 'maps/custom_2.txt';
+
+% straight line = 1, sine = 2, arc = 3, combined = 4
+public_vars.path_type = 4;
