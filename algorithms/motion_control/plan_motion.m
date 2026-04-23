8cacc63e73bbc50a1972578f9c5b421ef3d44741
Vítězslav Žáček <62467545+Vitek8@users.noreply.github.com>
2026-04-23 14:59:04 +0200

    odstranění přídavných řádků v main.m

--- algorithms/motion_control/plan_motion.m
+++ algorithms/motion_control/plan_motion.m
@@ -1,17 +1,17 @@
-function [public_vars] = plan_motion(read_only_vars, public_vars)
+function [public_vars, index] = plan_motion(read_only_vars, public_vars, last_index)
 %PLAN_MOTION Summary of this function goes here
 
 % I. Pick navigation target
 lookahead = 0.5;
-target = get_target(read_only_vars.mocap_pose, public_vars.path, lookahead);
+est_pos = public_vars.estimated_pose;
+[target, index] = get_target(est_pos, public_vars.path, lookahead, last_index);
+plot(target(1), target(2), 'bo', 'MarkerSize',8, 'MarkerFaceColor','b')
 if size(target) > 0
     public_vars.motion_vector = [0, 0];
     
     % II. Compute motion vector
     Kp = 5; 
     wheel_distance = read_only_vars.agent_drive.interwheel_dist;
-    
-    est_pos = read_only_vars.mocap_pose;
     
     x = est_pos(1);
     y = est_pos(2);
@@ -27,12 +27,17 @@ if size(target) > 0
         angle_error = 0;
     end
     
+    v_max = read_only_vars.agent_drive.max_vel;
+    v_min = -read_only_vars.agent_drive.max_vel;
+
     omega_rad = deg2rad(angle_error);
     omega_setpoint = Kp * omega_rad;
     v = sqrt(dx^2 + dy^2);
+    v = max(min(v,  v_max), v_min);
     
     v_left = v - (omega_setpoint * wheel_distance)/ 2;
     v_right = v + (omega_setpoint * wheel_distance) / 2;
+
     public_vars.motion_vector = [v_right, v_left];
 end
 end
