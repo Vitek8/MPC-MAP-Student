38409b44c31eeae0c35528c860dfec3f09f35786
Vítězslav Žáček <62467545+Vitek8@users.noreply.github.com>
2026-04-23 12:35:20 +0200

    odstranění nepotřebných algoritmů pro KF a EKF

--- algorithms/student_workspace.m
+++ algorithms/student_workspace.m
@@ -1,16 +1,9 @@
 function [public_vars] = student_workspace(read_only_vars,public_vars)
 %STUDENT_WORKSPACE Summary of this function goes here
 
-% 8. Perform initialization procedure
-if (read_only_vars.counter == 1)
-          
-    public_vars = init_particle_filter(read_only_vars, public_vars);
-    % public_vars = init_kalman_filter(read_only_vars, public_vars);
-
-end
-
 gnss_count = 100;
 gnss_tmp = gnss_count + 1;
+public_vars.motion_vector = [0, 0];
 
 if read_only_vars.counter < gnss_tmp
 
@@ -18,31 +11,18 @@ if read_only_vars.counter < gnss_tmp
 
 elseif read_only_vars.counter == gnss_tmp
 
-    gnss  = public_vars.gnss_data  - mean(public_vars.gnss_data);
+    gnss  = public_vars.gnss_data  - mean(read_only_vars.gnss_history);
     public_vars.gnss_sigma = std(gnss);
     public_vars.gnss_covariance = cov(gnss);
     public_vars = init_kalman_filter(read_only_vars, public_vars);
 
 elseif read_only_vars.counter < gnss_tmp * 4
+
     public_vars.motion_vector = [0.4, 0.4];
-else
-    public_vars.motion_vector = [0, 0];
-end
-% 9. Update particle filter
-public_vars.particles = update_particle_filter(read_only_vars, public_vars);
 
-% 10. Update Kalman filter
 if read_only_vars.counter > gnss_tmp
-    [public_vars.mu, public_vars.sigma, public_vars] = update_kalman_filter(read_only_vars, public_vars);
+    [public_vars.mu, public_vars.sigma] = update_kalman_filter(read_only_vars, public_vars);
+    public_vars.estimated_pose = estimate_pose(public_vars);
 end
-% 11. Estimate current robot position
-public_vars.estimated_pose = estimate_pose(public_vars); % (x,y,theta)
-
-% 12. Path planning
-% public_vars.path = plan_path(read_only_vars, public_vars);
-
-% 13. Plan next motion command
-% public_vars = plan_motion(read_only_vars, public_vars);
-
 
 end
