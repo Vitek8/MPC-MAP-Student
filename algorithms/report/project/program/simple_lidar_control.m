6dc340527f1111f625bee22d873e3859bf112ce9
Vítězslav Žáček <62467545+Vitek8@users.noreply.github.com>
2026-05-01 21:57:49 +0200

    přidána funkce pro jízdu robota pouze z lidaru

--- algorithms/report/project/program/simple_lidar_control.m
+++ algorithms/report/project/program/simple_lidar_control.m
@@ -1,28 +1,27 @@
 function public_vars = simple_lidar_control(read_only_vars, public_vars)
+
 lidar = read_only_vars.lidar_distances;
 wheel_distance = read_only_vars.agent_drive.interwheel_dist;
 
-lidar_front = lidar(1);
-right_lidar = mean(lidar(7:8));
-left_lidar  = mean(lidar(2:3));
+front = lidar(1);
+left  = mean(lidar(2:3));
+right = mean(lidar(7:8));
 
-d_safe = 1;
-k_turn = 0.2;
-v_max  = 0.3;
+v = 0.3;
+k_turn = 0.3;
+d_stop = 0.5;
 
-error = left_lidar - right_lidar;
+error = left - right;
 omega = k_turn * error;
 
-if lidar_front < d_safe
+if front < d_stop
     v = 0;
-    omega = 0.2;
-else
-    v = v_max;
+    omega = 0.5 * sign(error + 0.01);
 end
 
-v_left = v - (omega * wheel_distance)/ 2;
-v_right = v + (omega * wheel_distance) / 2;
+v_left  = v - (omega * wheel_distance)/2;
+v_right = v + (omega * wheel_distance)/2;
 
-public_vars.motion_vector = [v_right, v_left]; 
+public_vars.motion_vector = [v_right, v_left];
 
 end
