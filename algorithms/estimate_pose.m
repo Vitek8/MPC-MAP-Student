44b9fbaf0d668b174e028213b58cf4db979e4a86
Vítězslav Žáček <62467545+Vitek8@users.noreply.github.com>
2026-05-01 21:13:35 +0200

    přidána proměnná pro výběr estimace pózy na základě aktivního algoritmu

--- algorithms/estimate_pose.m
+++ algorithms/estimate_pose.m
@@ -1,9 +1,10 @@
-function [estimated_pose] = estimate_pose(public_vars, algorithms)
-
-if algorithms == "kf"
+function [estimated_pose] = estimate_pose(public_vars)
+if public_vars.kf_enabled
     estimated_pose = [public_vars.kf.mu(1), public_vars.kf.mu(2), public_vars.kf.mu(3)];
-else 
+elseif  public_vars.pf_enabled
     estimated_pose = [public_vars.pf.mu(1), public_vars.pf.mu(2), public_vars.pf.mu(3)];
+else
+    estimated_pose = [];
 end
 
 end
