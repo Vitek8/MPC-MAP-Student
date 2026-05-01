71ae6b318f1c2dc7ea0c91a13cf24eff0c6d0806
Vítězslav Žáček <62467545+Vitek8@users.noreply.github.com>
2026-05-01 21:54:02 +0200

    odstranění NaN průsečíků z ray_cast

--- algorithms/particle_filter/compute_lidar_measurement.m
+++ algorithms/particle_filter/compute_lidar_measurement.m
@@ -1,17 +1,19 @@
 function [measurement] = compute_lidar_measurement(map, pose, lidar_config)
-%COMPUTE_MEASUREMENTS Summary of this function goes here
 
 measurement = zeros(1, length(lidar_config));
-for i=1:length(lidar_config)
+
+for i = 1:length(lidar_config)
+    
     phi = pose(3) + lidar_config(i);
     intersections = ray_cast(pose(1:2), map.walls, phi);
+    intersections = intersections(~any(isnan(intersections),2), :);
+
     if isempty(intersections)
-        measurement(i) = 10;
+        measurement(i) = 20;
     else
-        distances = sqrt((intersections(:,1) - pose(1)).^2 + (intersections(:,2) - pose(2)).^2);
+        distances = sqrt((intersections(:,1) - pose(1)).^2 + (intersections(:,2) - pose(2)).^2); 
         measurement(i) = min(distances);
     end
 end
 
 end
-
