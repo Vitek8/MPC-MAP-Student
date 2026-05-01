6ab63b51a8e09ca134a774d5b8f960d43f8c5a8f
Vítězslav Žáček <62467545+Vitek8@users.noreply.github.com>
2026-05-01 21:24:30 +0200

    náhrada knihovního prvku 'wrapToPi'

--- algorithms/motion_control/plan_motion.m
+++ algorithms/motion_control/plan_motion.m
@@ -5,8 +5,8 @@ function [public_vars] = plan_motion(read_only_vars, public_vars)
 lookahead = 0.5;
 est_pos = public_vars.estimated_pose;
 [target, public_vars] = get_target(est_pos, public_vars.path, lookahead, public_vars);
-% plot(target(1), target(2), 'bo', 'MarkerSize',8, 'MarkerFaceColor','b')
 if size(target) > 0
+    % plot(target(1), target(2), 'bo', 'MarkerSize',8, 'MarkerFaceColor','b')
     public_vars.motion_vector = [0, 0];
     
     % II. Compute motion vector
@@ -18,19 +18,19 @@ if size(target) > 0
     dx = target(1) - x;
     dy = target(2) - y;
     
-    theta = (rad2deg(wrapToPi(est_pos(3))));
-    target_angle = rad2deg(atan2(dy, dx));
-    
+    theta = mod(est_pos(3) + pi, 2*pi) - pi;
+    target_angle = atan2(dy, dx);
     angle_error = target_angle - theta;
     
-    if abs(angle_error) < 5
+    % pi/36 rad = 5 deg
+    if abs(angle_error) < pi/36    
         angle_error = 0;
     end
     
     v_max = read_only_vars.agent_drive.max_vel;
     v_min = -read_only_vars.agent_drive.max_vel;
 
-    omega_rad = deg2rad(angle_error);
+    omega_rad = angle_error;
     omega_setpoint = Kp * omega_rad;
     v = sqrt(dx^2 + dy^2);
     v = max(min(v,  v_max), v_min);
