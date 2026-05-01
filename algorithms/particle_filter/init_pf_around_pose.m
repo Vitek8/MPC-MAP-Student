872bcff0f21d77a9b7ba425f73baa9effcbd22c1
Vítězslav Žáček <62467545+Vitek8@users.noreply.github.com>
2026-05-01 21:58:52 +0200

    přidána funkce pro inicializaci částic v poloze robota

---
+++ algorithms/particle_filter/init_pf_around_pose.m
@@ -0,0 +1,16 @@
+function public_vars = init_pf_around_pose(public_vars, pose)
+N = 200;
+sigma_x = 0.5;
+sigma_y = 0.5;
+sigma_theta = 5*pi/180;
+
+public_vars.particles(:,1) = pose(1) + sigma_x * randn(N,1);
+public_vars.particles(:,2) = pose(2) + sigma_y * randn(N,1);
+public_vars.particles(:,3) = pose(3) + sigma_theta * randn(N,1);
+public_vars.particles(:,3) = atan2(sin(public_vars.particles(:,3)), cos(public_vars.particles(:,3)));
+public_vars.weights = ones(N, 1) / N;
+
+public_vars.motion_sigma = 0.75;
+public_vars.sensor_sigma = 4;
+
+end
