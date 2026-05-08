ac0a6b009f1154d889a25452d8e65b6a6d5ac7bb
Vítězslav Žáček <62467545+Vitek8@users.noreply.github.com>
2026-05-08 22:28:55 +0200

    lehký tuning pf

--- algorithms/particle_filter/update_particle_filter.m
+++ algorithms/particle_filter/update_particle_filter.m
@@ -21,9 +21,9 @@ public_vars.weights = weight_particles(measurements, lidar, public_vars.sensor_s
 Neff = 1 / sum(public_vars.weights.^2);
 particles = resample_particles(particles, public_vars.weights);
 if Neff < N/2
-    particles(:,1) = particles(:,1) + 0.05 * randn(size(particles,1),1);
-    particles(:,2) = particles(:,2) + 0.05 * randn(size(particles,1),1);
-    particles(:,3) = particles(:,3) + 0.05 * randn(size(particles,1),1);
+    particles(:,1) = particles(:,1) + 0.08 * randn(size(particles,1),1);
+    particles(:,2) = particles(:,2) + 0.08 * randn(size(particles,1),1);
+    particles(:,3) = particles(:,3) + 0.08 * randn(size(particles,1),1);
 end
 
 % IV. Pose estimation
